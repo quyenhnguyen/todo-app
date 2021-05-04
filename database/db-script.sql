8bca6cc8ab292aa93f0e95caad73ea1c6d9a2f6d
quyennguyen <hoangquyen7901@gmail.com>
2021-05-04 15:37:51 +0700

    initialize database script

---
+++ database/db-script.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE "todo-app-db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE TABLE public.account
+(
+    id integer NOT NULL,
+    email text COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default",
+    CONSTRAINT account_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.account
+    OWNER to postgres;
+
+
+CREATE TABLE public.task
+(
+    id integer NOT NULL,
+    summary text COLLATE pg_catalog."default",
+    status boolean,
+    acc_id integer NOT NULL,
+    CONSTRAINT task_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_task_account FOREIGN KEY (acc_id)
+        REFERENCES public.account (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.task
+    OWNER to postgres;
