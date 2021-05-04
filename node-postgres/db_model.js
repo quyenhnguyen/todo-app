c7b9c17761bd86248321dff1511d709f401c39a0
quyennguyen <hoangquyen7901@gmail.com>
2021-05-04 15:41:49 +0700

    api initialization

---
+++ node-postgres/db_model.js
@@ -0,0 +1,108 @@
+//directly query to database
+const Pool =require('pg').Pool
+const pool=new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'todo-app-db',
+    password: '30112',
+    port: 5432
+})
+
+const getAllUserAccount=()=>{
+    return new Promise(function (resolve, reject){
+        pool.query('SELECT * FROM account ORDER BY id ASC',(error, results)=>{
+            if(error){
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const createUserAccount=(body)=>{
+    return new Promise(function(resolve, reject){
+        console.log(body)
+        const {id, email, password}=body
+        sqlQuery=`INSERT INTO account(id, email, password) VALUES (${id},'${email}', '${password}')`
+        console.log(sqlQuery)
+        pool.query(sqlQuery, (error, results)=>{
+            if(error)
+            {
+                console.log(error)
+                reject(error)
+            }
+            resolve(results)
+
+        })
+    })
+}
+
+const getUserTodo=(user_id)=>{
+    return new Promise(function(resolve, reject){
+        const id = parseInt(user_id)
+        sqlQuery=`SELECT * FROM task WHERE acc_id=${id}`
+        console.log(sqlQuery)
+        pool.query(sqlQuery, (error, results)=>{
+            if(error)
+            {
+                reject(error)
+            }
+            resolve(results.rows)
+
+        })
+    })
+    
+}
+
+const createUserTodo=(body)=>{
+    return new Promise(function(resolve, reject){
+        const {id, summary, status, acc_id}=body
+        sqlQuery=`INSERT INTO task(id, summary, status, acc_id) VALUES (${id}, '${summary}', ${status}, ${acc_id})`
+        pool.query(sqlQuery, (error, results)=>{
+            if(error)
+            {
+                reject(error)
+            }
+            resolve(`A new to do have been added to user ${acc_id}`)
+
+        })
+    })
+}
+const deleteUserTodo=(task_id)=>{
+    return new Promise(function(resolve, reject){
+        const id = parseInt(task_id)
+        sqlQuery=`DELETE FROM task WHERE id = ${id}`
+        pool.query(sqlQuery, (error, results)=>{
+            if(error)
+            {
+                reject(error)
+            }
+            resolve(`Successfull`)
+
+        })
+    })
+}
+const updateUserTodo=(body)=>{
+    return new Promise(function(resolve, reject){
+        const {id, summary, status}=body
+        sqlQuery=`UPDATE task SET summary = ${summary}, status = ${status} WHERE id=${id}`
+        pool.query(sqlQuery, (error, results)=>{
+            if(error)
+            {
+                reject(error)
+            }
+            resolve(`Successfull`)
+
+        })
+    })
+}
+
+
+module.exports={
+    getAllUserAccount,
+    createUserAccount,
+    createUserTodo,
+    deleteUserTodo,
+    updateUserTodo,
+    getUserTodo
+}
---
+++ node-postgres/index.js
@@ -0,0 +1,88 @@
+//tuong tac voi request de lay tham so truyen vao db, de query
+const { response } = require('express')
+const express=require('express')
+const app=express()
+const port =3001
+
+const db_model=require('./db_model')
+app.use(express.json())
+
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+})
+
+//khi nguoi dung goi api thi tra ve j
+//Get all users in db
+app.get('/users',(req,res)=>{
+    db_model.getAllUserAccount()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+//create new user
+app.post('/users', (req, res) => {
+    db_model.createUserAccount(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+//get all todo of 1 user
+app.get('/users/:id/tasks', (req, res) => {
+    console.log(req.params.id)
+    db_model.getUserTodo(req.params.id)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
+})
+
+//add 1 toto to list todo
+app.post('/tasks', (req, res) => {
+    db_model.createUserTodo(req.body)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
+})
+
+//delete 1 toto of todo list
+app.delete('/tasks/:task_id', (req, res) => {
+    db_model.deleteUserTodo(req.params.task_id)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
+})
+
+//update 1 toto of todo list
+app.put('/tasks/:task_id', (req, res) => {
+    db_model.createMerchant(req.body)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`App is running on port ${port}.`)
+})
+
